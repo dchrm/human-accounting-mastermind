5335d03656ccf0d443d975a6fa954e050df6f011
oshgMETIYvnCRSthTWmWGitopoziRt <105390304+oshgMETIYvnCRSthTWmWGitopoziRt@users.noreply.github.com>
2022-09-19 17:11:31 -0400

    updated to put each function in its own file

---
+++ add_records_to_sql_database.py
@@ -0,0 +1,12 @@
+def add_records_to_sql_database(array_of_rows,database,table,row_titles):
+
+    # check if list of rows is over the insert limit
+    if len(array_of_rows) > 1000:
+        
+        # split array into chunks of 1000
+
+
+        # for each array chunk, insert the rows to the database
+
+
+        pass
